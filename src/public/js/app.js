e9642ecbe511a6930abb6813eb881069462e294a
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-22 18:48:57 +0900

    #1.6

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,33 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message)=>{
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", ()=> {
+    console.log("Disconnected for server X");
+});
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    console.log(input.value);
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+
+/*
+setTimeout(()=>{
+    socket.send("hello fro the browser!");
+}, 10000);
+*/
--- src/server.js
+++ src/server.js
@@ -11,12 +11,32 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-console.log("hello");
+//console.log("hello");
 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server }); // 서버는 http, ws 2개의 protocol을 이해할 수있다.
+
+function onSocketClose() {
+    console.log("Discnnected from the Browser ❌");
+};
+
+const sockets = [];
+
+wss.on("connection",(socket)=> { // 커넥션이 생겼을때 socket으로 즉시 메세지를 보낸다
+
+    sockets.push(socket);
+
+    console.log("Connected to Browser ✅");
+    socket.on("close",onSocketClose);
+    socket.on("message", (message) => {
+        //socket.send(message.toString()); // 프론트에서 받은거 보내기
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
+
+    });
+    
+});
 
 server.listen(3000, handleListen);
