54b2e92e9542d55a66ec6a96b1e21372e797f5c9
NyeongB <kdtoaj14@ajou.ac.kr>
2022-03-23 22:01:35 +0900

    #2.7

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket)=>{
+    socket["nickname"] = "Anon";
     socket.onAny((event)=>{
         console.log(`Socket Event: ${event}`);
     });
@@ -28,7 +29,21 @@ wsServer.on("connection", (socket)=>{
         
         socket.join(roomName);
         done();
+        socket.to(roomName).emit("welcome", socket.nickname); // welcome의 event를 roomName에 있는 모든 사람들에게 emit
     }); 
+
+    socket.on("disconnecting", ()=>{
+        socket.rooms.forEach((room)=> socket.to(room).emit("bye"), socket.nickname);
+    });
+
+    socket.on("new_message", (msg, room, done)=>{
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    })
+
+    socket.on("nickname", (nickname)=>{
+        socket["nickname"] = nickname;
+    })
 });
 
 
